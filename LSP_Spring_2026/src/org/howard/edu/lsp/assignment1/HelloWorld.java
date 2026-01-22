1a873915455c49e86b391f327f8dba89723a6319
djenkins05 <dakarrijenkins@gmail.com>
2026-01-21 20:36:21 -0500

    First Commit

---
+++ LSP_Spring_2026/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	
+	/**
+	 * Name: Dakarri Jenkins 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
